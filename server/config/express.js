8b3a3137abcf61833840f55890a829a9ddf7ecad
roni <ron.coen@gmail.com>
2014-08-25 23:24:26 +0300

    fix allow origin to *

--- server/config/express.js
+++ server/config/express.js
@@ -20,7 +20,7 @@ var mongoStore = require('connect-mongo')(session);
 var mongoose = require('mongoose');
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', config.allowedDomains);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
